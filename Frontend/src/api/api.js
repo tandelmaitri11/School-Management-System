7ad150f8ae56dc024218f71a917eaf78fa99b2eb
tandelmaitri <tandelmaitri11@gmail.com>
2026-07-16 22:18:06 +0530

    frontend env file update.

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
-
 
 export default api;
